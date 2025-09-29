3cd1b56a9dfd141c1c8b7d5409444e9909638e72
Mouzam ali <malikmouzam316@gmail.com>
2025-09-29 12:45:28 +0500

    vite changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
-
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
@@ -10,6 +11,7 @@ export default defineConfig({
         target: "https://full-stack-backend-5-dm0x.onrender.com",
         changeOrigin: true, // ✅ Fixes host header issues
         secure: true,       // ✅ Ensures HTTPS is respected
+        rewrite: (path) => path.replace(/^\/api/, ""), // ⬅️ Add this line
       },
     },
   },
